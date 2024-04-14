e1ad23ba944a9d1d5fb84e4bb7f2802df3b5b1c2
Tarek-DG <63122614+Tarek-DG@users.noreply.github.com>
2024-04-14 23:17:10 +0100

    up

--- js/custom.js
+++ js/custom.js
@@ -186,3 +186,43 @@ $(document).ready(function () {
         });
     }, { offset: '60%' });
 });
+
+(function ($) {
+    var ssMobileMenu = function () {
+
+        var toggleButton = $('.header-menu-toggle'),
+            nav = $('.header-nav-wrap');
+
+        toggleButton.on('click', function (event) {
+            event.preventDefault();
+
+            toggleButton.toggleClass('is-clicked');
+            nav.slideToggle();
+        });
+
+        if (toggleButton.is(':visible')) nav.addClass('mobile');
+
+        $WIN.on('resize', function () {
+            if (toggleButton.is(':visible')) nav.addClass('mobile');
+            else nav.removeClass('mobile');
+        });
+
+        nav.find('a').on("click", function () {
+
+            if (nav.hasClass('mobile')) {
+                toggleButton.toggleClass('is-clicked');
+                nav.slideToggle();
+            }
+        });
+
+    };
+    (function ssInit() {
+
+
+        ssMobileMenu();
+
+
+    })();
+
+
+})(jQuery);
